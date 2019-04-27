acbf6bd6f490a04ffe2877010fd89fb2e5833fbd
Lindsey Wells <lindsey.wells84@gmail.com>
2019-04-27 10:50:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,19 @@
 array = ["fiddleheads","okra","kohlrabi"]
 
+#def oxford_comma(array)
+#if array.length == 1
+#array.pop
+#elsif array.length == 2
+#"#{array[0]} and #{array.pop}"
+#elsif array.length == 3
+#"#{array[0]}, #{array[1]}, and #{array.pop}"
+#end
+#end
+
 def oxford_comma(array)
-if array.length == 1
-array.pop
-elsif array.length == 2
-"#{array[0]} and #{array.pop}"
-elsif array.length == 3
-"#{array[0]}, #{array[1]}, and #{array.pop}"
-end
-end
+  with_commas = []
+  array([0]...[-1]).each do |string|
+    with_commas<< "#{string}, "
+  end
+if array.length  
+

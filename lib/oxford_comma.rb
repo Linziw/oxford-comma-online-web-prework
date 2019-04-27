98a75026ec9f890e0441431ce2cc3815d57b456a
Lindsey Wells <lindsey.wells84@gmail.com>
2019-04-27 10:39:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ array = ["fiddleheads","okra","kohlrabi"]
 def oxford_comma(array)
 if array.length == 1
 array.pop
-#elsif array.length == 2
-#puts "#{array[0]} and #{last}"
+elsif array.length == 2
+puts "#{array[0]} and #{array.pop}"
 end
 end

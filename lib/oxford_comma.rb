a863e742cf459860bac2dc6a0dd966ea177cddf3
Lindsey Wells <lindsey.wells84@gmail.com>
2019-04-27 10:43:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,8 @@ def oxford_comma(array)
 if array.length == 1
 array.pop
 elsif array.length == 2
-"#{array[0]} and #{array.pop}" 
+"#{array[0]} and #{array.pop}"
+elsif array.length == 3
+""
 end
 end
